6cc13364a7e966bf009792ba486ba88dcc77f51a
Robert Fodor <ingrobertfodor@gmail.com>
2023-08-07 10:55:52 +0200

    New Script 19_web

---
+++ practicepython/19_decode_a_web_page_two.py
@@ -0,0 +1,72 @@
+# WEB TA PURPOSES
+from selenium import webdriver
+# FINDING ELEMENTS PURPOSES
+from selenium.webdriver.common.by import By
+# FINDING ELEMENTS PURPOSES
+# !!! RUN THIS FIRST (CMD)
+# pip install htmldom==2.0
+from htmldom import htmldom
+# TEXT PURPOSES
+from selenium.webdriver.common.keys import Keys
+# WAIT PURPOSES
+from selenium.webdriver.support.wait import WebDriverWait
+# WAIT PURPOSES
+# Shortened version of code (as).
+from selenium.webdriver.support import expected_conditions as EC
+# SLEEP PURPOSES
+import time
+# DATE TIME PURPOSES
+import datetime
+# DROPDOWN PURPOSES
+from selenium.webdriver.support.select import Select
+# SLIDER PURPOSES - IN THIS SCRIPT
+from selenium.webdriver.common.action_chains import ActionChains
+# RANDOM PURPOSES
+# https://www.w3schools.com/python/module_random.asp
+# Import the random module, and display a random number between 1 and 9:
+import random
+# STRING PURPOSES
+import string
+# MATH PURPOSES
+import math
+# MATH PURPOSES - Fractions
+import fractions
+# SEARCH PURPOSES (RegEx)
+import re
+# DELETE, FILE HANDLING PURPOSES
+import os
+# MATPLOTLIB, VISUALIZATION PURPOSES
+import matplotlib
+# MYSQL PURPOSES
+import mysql.connector
+# REQUESTS PURPOSES
+# !!! RUN THIS FIRST (CMD)
+# pip install requests
+import requests
+# BEAUTIFUL SOUP PURPOSES
+# !!! RUN THIS FIRST (CMD)
+# pip install bs4
+from bs4 import BeautifulSoup
+
+
+driver = webdriver.Chrome()
+try:
+    # This is just test to open the browser.
+    driver.get("https://www.nytimes.com")
+    
+    # These steps do things for this exercise.
+    web_page_url = "https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
+    web_page_displayed = requests.get(web_page_url)
+    print(web_page_displayed.url)
+    html = web_page_displayed.content
+    soup = BeautifulSoup(html, features="html.parser")
+    
+    # This prints the web page text.
+    for t in soup.find_all("div", class_ = "body__inner-container"):
+        print(t.text)
+    # Empty row.
+    print()
+
+finally:
+    # This is just test to close the browser.
+    driver.quit()
