b418ca782ebb15eadb580e82d8308ea44256e58e
Robert Fodor <ingrobertfodor@gmail.com>
2023-08-10 11:11:27 +0200

    22 Scripts

---
+++ practicepython/22_read_from_file.py
@@ -0,0 +1,73 @@
+# WEB TA PURPOSES
+from selenium import webdriver
+# FINDING ELEMENTS PURPOSES
+from selenium.webdriver.common.by import By
+# FINDING ELEMENTS PURPOSES
+# !!! RUN THIS FIRST (CMD)
+# pip install htmldom==2.0
+from htmldom import htmldom
+# TEXT PURPOSES
+from selenium.webdriver.common.keys import Keys
+# WAIT PURPOSES
+from selenium.webdriver.support.wait import WebDriverWait
+# WAIT PURPOSES
+# Shortened version of code (as).
+from selenium.webdriver.support import expected_conditions as EC
+# SLEEP PURPOSES
+import time
+# DATE TIME PURPOSES
+import datetime
+# DROPDOWN PURPOSES
+from selenium.webdriver.support.select import Select
+# SLIDER PURPOSES - IN THIS SCRIPT
+from selenium.webdriver.common.action_chains import ActionChains
+# RANDOM PURPOSES
+# https://www.w3schools.com/python/module_random.asp
+# Import the random module, and display a random number between 1 and 9:
+import random
+# STRING PURPOSES
+import string
+# MATH PURPOSES
+import math
+# MATH PURPOSES - Fractions
+import fractions
+# SEARCH PURPOSES (RegEx)
+import re
+# DELETE, FILE HANDLING PURPOSES
+import os
+# MATPLOTLIB, VISUALIZATION PURPOSES
+import matplotlib
+# MYSQL PURPOSES
+import mysql.connector
+# REQUESTS PURPOSES
+# !!! RUN THIS FIRST (CMD)
+# pip install requests
+import requests
+# BEAUTIFUL SOUP PURPOSES
+# !!! RUN THIS FIRST (CMD)
+# pip install bs4
+from bs4 import BeautifulSoup
+
+
+my_dictionary = {}
+
+with open("test.txt", "r") as open_file:
+    
+    # This would read and print the whole file.
+    # all_text = open_file.read()
+    # print(all_text)
+
+    # This reads and prints the whole file line by line.
+    line = open_file.readline()
+    
+    while line:
+        line_categories = line[3:-26]
+        print(line_categories)
+        # This counts the categories (e.g.: food_court : 50)
+        if line_categories in my_dictionary:
+            my_dictionary[line_categories] +=1
+        else:
+            my_dictionary[line_categories] =1
+        line = open_file.readline()
+        
+print(my_dictionary)
