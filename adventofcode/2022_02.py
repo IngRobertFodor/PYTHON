b383326e7d8f902759f093943951afc61b770eb8
Robert Fodor <ingrobertfodor@gmail.com>
2023-09-15 16:57:23 +0200

    Update

--- adventofcode/2022_02.py
+++ adventofcode/2022_02.py
@@ -7,49 +7,94 @@ with open("2022_02.txt", "r") as open_file:
     print(lines)
     print(len(lines))
     # This is my opponent (A, B, C).
-    keys_list = []
+    my_opponent = []
     # This is me (X, Y, Z).
-    values_list = []
-    
+    my_moves = []
     for i in range(0,len(lines)):
-        keys = lines[i][0:1]
-        keys_list.append(keys)
-        values = lines[i][2:3]
-        values_list.append(values)
+        opponent = lines[i][0:1]
+        my_opponent.append(opponent)
+        me = lines[i][2:3]
+        my_moves.append(me)
     # This is my opponent (A, B, C).
-    print(keys_list)
-    print(collections.Counter(keys_list))
+    print(my_opponent)
+    print(collections.Counter(my_opponent))
     # This is me (X, Y, Z).
-    print(values_list)
-    print(collections.Counter(values_list))
+    print(my_moves)
+    print(collections.Counter(my_moves))
 
-    # This counts moves of each player.
-    # This is my opponent (A, B, C).
-    a = keys_list.count("A")
+    # This will count moves of each player.
+    # These are my opponent's moves (A, B, C).
+    a = my_opponent.count("A")
     print(a)
     a = a*1
     print(a)
-    b = keys_list.count("B")
+    b = my_opponent.count("B")
     print(b)
     b = b*2
     print(b)
-    c = keys_list.count("C")
+    c = my_opponent.count("C")
     print(c)
     c = c*3
     print(c)
-    # This is me (X, Y, Z).
-    x = values_list.count("X")
+    # These are my moves (X, Y, Z).
+    x = my_moves.count("X")
     print(x)
     x = x*1
     print(x)
-    y = values_list.count("Y")
+    y = my_moves.count("Y")
     print(y)
     y = y*2
     print(y)
-    z = values_list.count("Z")
+    z = my_moves.count("Z")
     print(z)
     z = z*3
     print(z)
 
-    print(f"This is sum (just for moves) for my opponent: {a+b+c}.")
-    print(f"This is sum (just for moves) for me: {x+y+z}.")
+## This is sum (just for moves).
+    print("First part of results.")
+    print(f"This is sum of points (just for moves) for my opponent: {a+b+c}.")
+    print(f"This is sum of points (just for moves) for me: {x+y+z}.")
+    print()
+
+    
+    # These will be base data to calculate sum for loses, draws or wins (1, 3, 6).
+    for i in range(0,len(my_opponent)):
+        if my_opponent[i] == "A":
+            my_opponent[i] = "1"
+        elif my_opponent[i] == "B":
+            my_opponent[i] = "2"
+        elif my_opponent[i] == "C":
+            my_opponent[i] = "3"
+    print(my_opponent)
+    print(len(my_opponent))
+    for j in range(0,len(my_moves)):
+        if my_moves[j] == "X":
+            my_moves[j] = "1"
+        elif my_moves[j] == "Y":
+            my_moves[j] = "2"
+        elif my_moves[j] == "Z":
+            my_moves[j] = "3"
+    print(my_moves)
+    print(len(my_moves))
+
+    # This will be sum for draws. 
+    my_opponent_draws = my_opponent.copy()
+    my_moves_draws = my_moves.copy()
+    for i in range(0, len(my_opponent_draws)):
+        for i in range(0, len(my_moves_draws)):
+            if my_opponent_draws[i] == my_moves_draws[i]:
+                my_opponent_draws[i] = "draw"  
+                my_moves_draws[i] = "draw"       
+    print(my_opponent_draws)
+    print(my_moves_draws)
+    results_draws = []
+    for item in my_opponent_draws:
+        if item == "draw":
+            results_draws.append(item)
+    print(results_draws)
+    print(len(results_draws))
+## These are points for draws.
+    print("Second part of results.")
+    draw_points = len(results_draws) * 3
+    print(f"For draw are same points for my opponent and me: {draw_points}.")
+    print()
