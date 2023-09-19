fe3737be416a21b7a99b3b750be5e7fe8068baca
Robert Fodor <ingrobertfodor@gmail.com>
2023-09-19 20:21:39 +0200

    Update

---
+++ adventofcode/2022_03.py
@@ -0,0 +1,63 @@
+import string
+
+
+with open("2022_03.txt", "r") as open_file:
+    
+    lines = open_file.readlines()
+    print(lines)
+    print(len(lines))
+    
+    list_items = []
+    for i in range(0,len(lines)):
+        item = lines[i].strip()
+        list_items.append(item)
+    print(list_items)
+
+    elf_items = []
+    for item in list_items:
+        n = len(item)
+        print(n)
+        if n%2 == 0:
+            string_one = set(item[0:n//2])
+            string_two = set(item[n//2:])
+            print("First Half of String:",string_one)
+            print("Second Half of String:",string_two)
+            unique = set(string_one).intersection(set(string_two))
+            print(f" This is item from backpack that is unique: {unique}.")
+            elf_items.append(unique)
+        else:
+            string_one = set(item[0:(n//2+1)])
+            string_one = set(item[(n//2+1):])
+            print("First Half of String:",string_one)
+            print("Second Half of String:",string_two)
+            unique = set(string_one).intersection(set(string_two))
+            print(f" This is item from backpack that is unique: {unique}.")
+            elf_items.append(unique)
+    print(elf_items)
+
+    elf_items_strings_first = []
+    for i in elf_items:
+        i = str(i)
+        string_i = i.replace("{'", "")
+        elf_items_strings_first.append(string_i)
+    print(elf_items_strings_first)
+    elf_items_strings_second = []
+    for i in elf_items_strings_first:
+        i = str(i)
+        string_i = i.replace("'}", "")
+        elf_items_strings_second.append(string_i)
+    print(elf_items_strings_second)
+    elf_items_strings_second
+    elf_items_strings = elf_items_strings_second
+    print(elf_items_strings)
+
+    elf_items_strings_lower_case = []
+    elf_items_strings_upper_case = []
+    for i in elf_items_strings:
+        if i in string.ascii_lowercase:
+            elf_items_strings_lower_case.append(i)
+        else:
+            elf_items_strings_upper_case.append(i)
+    print()
+    print(elf_items_strings_lower_case)
+    print(elf_items_strings_upper_case)
