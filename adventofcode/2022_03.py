ead5db41811fbf8eac8d02773349da80342eaaa2
Robert Fodor <ingrobertfodor@gmail.com>
2023-09-20 13:51:16 +0200

    Update

--- adventofcode/2022_03.py
+++ adventofcode/2022_03.py
@@ -61,3 +61,21 @@ with open("2022_03.txt", "r") as open_file:
     print()
     print(elf_items_strings_lower_case)
     print(elf_items_strings_upper_case)
+
+    # 52 numbers and 52 letters
+    numbers = list(range(1,53))
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    priority_values = dict(zip(letters, numbers))
+    print(priority_values)
+    k = list(priority_values.keys())
+    v = list(priority_values.values())
+    print(k)
+    print(v)
+
+    xx = []
+    for i in elf_items_strings:
+        if i in priority_values.keys():
+            x = priority_values.get(i)
+            xx.append(x)
+    print(xx)
+    print(f"This is sum of priority values in backpack: {sum(xx)}.")
