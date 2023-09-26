84a5500723950498049128351fd18c6269b70561
Robert Fodor <ingrobertfodor@gmail.com>
2023-09-26 23:00:21 +0200

    Update

--- adventofcode/2022_04_first_part.py
+++ adventofcode/2022_04_first_part.py
@@ -1,6 +1,7 @@
 #1 In how many assignment pairs does one range fully contain the other?
 
 import string
+import collections
 
 
 with open("2022_04.txt", "r") as open_file:
@@ -15,8 +16,43 @@ with open("2022_04.txt", "r") as open_file:
         list_items.append(item)
     print(list_items)
 
-    list_items_splitted = []
-    chunk_size = 2
-    for i in range(0, len(list_items), chunk_size):
-        list_items_splitted.append(list_items[i:i+chunk_size])
-    print(list_items_splitted)
+    split_list_items = []
+    for i in range(0, len(list_items)):
+        split = list_items[i].split(",")
+        split_list_items.append(split)
+    print(split_list_items)
+
+    sub = []
+    for i in range(0,len(split_list_items)):
+        # print(split_list_items[i][0])
+        a = split_list_items[i][0].split("-")[0]
+        z = split_list_items[i][0].split("-")[1]
+        a = int(a)
+        z = int(z)
+        print(a)
+        print(z)
+        range_a_z = list(range(a,z+1))
+        print(range_a_z)
+        # print(split_list_items[i][1])
+        aa = split_list_items[i][1].split("-")[0]
+        zz = split_list_items[i][1].split("-")[1]
+        aa = int(aa)
+        zz = int(zz)
+        print(aa)
+        print(zz)
+        range_aa_zz = list(range(aa,zz+1))
+        print(range_aa_zz)
+        sub_one = set(range_a_z).issuperset(set(range_aa_zz))
+        if sub_one == True:
+            sub.append(sub_one)
+        sub_two = set(range_aa_zz).issuperset(set(range_a_z))
+        if sub_two == True:
+            sub.append(sub_two)
+        if range_a_z == range_aa_zz:
+            sub.append("Adjust")        
+        print(sub)
+
+        print()
+        print("Result is sum of counted 'True' values minus number of counted 'Adjust' values.")
+        print(collections.Counter(sub))
+        print("Result is 536.")
