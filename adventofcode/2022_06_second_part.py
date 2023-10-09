fc03989dbf647426e1af40a095a50c2da675402c
Robert Fodor <ingrobertfodor@gmail.com>
2023-10-09 13:43:03 +0200

    Script

--- adventofcode/2022_06_second_part.py
+++ adventofcode/2022_06_second_part.py
@@ -1,4 +1,4 @@
-#1 How many characters need to be processed before the first start-of-packet marker is detected?
+#2 How many characters need to be processed before the first start-of-packet marker is detected?
 
 
 with open("2022_06.txt", "r") as open_file:
@@ -11,13 +11,13 @@ with open("2022_06.txt", "r") as open_file:
         text_list.append(i)
     print(text_list)
 
-    chunksize = 4
+    chunksize = 14
     list_characters = [text_list[i: i + chunksize] for i in range(len(text_list) + 1 - chunksize)]
     print(list_characters)
 
     my_string_in_list = []
     for i in list_characters:
-        if len(set(i)) == 4:
+        if len(set(i)) == 14:
             my_string_in_list.append(i)
             break
     print(my_string_in_list)
@@ -29,5 +29,5 @@ with open("2022_06.txt", "r") as open_file:
     my_string_position = text.find(str(my_string_converted_to_string))
     print(f'This is position of my string: {my_string_position}.')
 
-    number_to_find_is = my_string_position+4
+    number_to_find_is = my_string_position+14
     print(f'This is number to find: {number_to_find_is}.')
