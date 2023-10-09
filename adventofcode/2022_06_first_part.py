dee902c5ca1eacd44341e123acbb190ab51c5523
Robert Fodor <ingrobertfodor@gmail.com>
2023-10-09 09:05:20 +0200

    Scripts

---
+++ adventofcode/2022_06_first_part.py
@@ -0,0 +1,9 @@
+#1 How many characters need to be processed before the first start-of-packet marker is detected?
+
+
+with open("2022_06.txt", "r") as open_file:
+    
+    text = open_file.read()
+    print(text)
+
+    
