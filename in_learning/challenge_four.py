ce7d8d04af8a92ef5bb2d1ff7a48af3f6e6bc73b
Robert Fodor <ingrobertfodor@gmail.com>
2023-10-19 11:44:11 +0200

    Update

--- in_learning/challenge_four.py
+++ in_learning/challenge_four.py
@@ -1,3 +1,18 @@
+import collections
+
+
+my_string = "AAABBCCCCD"
+print(my_string)
+my_count = collections.Counter(my_string)
+print(type(my_count))
+my_count = dict(my_count)
+print(type(my_count))
+print(my_count)
+test_dict_get = my_count.get("B")
+print(test_dict_get)
+print()
+
+
 art = '''
 
                                                                                 
@@ -36,8 +51,15 @@ art = '''
                                                                                  
 
 '''
+print(art)
 art_list = art.split()
 print(art_list)
 
 art.encode("utf-8")
-print(art)
+#print(art)
+
+my_count_art = collections.Counter(art)
+print(type(my_count_art))
+my_count_art = dict(my_count_art)
+print(type(my_count_art))
+print(my_count_art)
