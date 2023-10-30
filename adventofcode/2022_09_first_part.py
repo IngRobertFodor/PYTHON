2db06f89cab46c93da6566fdd6d3a930580f8c5a
Robert Fodor <ingrobertfodor@gmail.com>
2023-10-30 18:00:09 +0100

    Update

--- adventofcode/2022_09_first_part.py
+++ adventofcode/2022_09_first_part.py
@@ -21,30 +21,27 @@ print()
 #########################################################################################
 
 
-def read_file():
-    with open("2022_09.txt", "r") as open_file:
+with open("2022_09.txt", "r") as open_file:
     
-        # Read file as "dictionary".
-        my_dict = {}
-        for line in open_file:
-            x = line.split()
-            my_dict.update({x[0].strip() : x[1].strip()})
-        #print(my_dict)
-        # These are moves of the letters one by one.
-        for key in my_dict:
-            if key == "R":
-                print("You have to move RIGHT " + my_dict[key] + " steps.")
-            elif key == "L":
-                print("You have to move LEFT " + my_dict[key] + " steps.")
-            elif key == "D":
-                print("You have to move DOWN " + my_dict[key] + " steps.")
-            else:
-                print("You have to move UP " + my_dict[key] + " steps.")
-        print()
-    return my_dict
+    # Read file as "dictionary".
+    my_dict = {}
+    for line in open_file:
+        x = line.split()
+        my_dict.update({x[0].strip() : x[1].strip()})
+    #print(my_dict)
+    # These are moves of the letters one by one.
+    for key in my_dict:
+        if key == "R":
+            print("You have to move RIGHT " + my_dict[key] + " steps.")
+        elif key == "L":
+            print("You have to move LEFT " + my_dict[key] + " steps.")
+        elif key == "D":
+            print("You have to move DOWN " + my_dict[key] + " steps.")
+        else:
+            print("You have to move UP " + my_dict[key] + " steps.")
+    print()
+    
 
-
-def original_board():
     my_list = [
     ['.', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.'],
@@ -54,26 +51,25 @@ def original_board():
     ]
     for item in my_list:
         print(item)
-    return my_list
-
-
-def find_moves():
-    # Iterate over each row.
-    for i in range(len(default_spot)):
-        # Iterate over each column.
-        for j in range(len(default_spot[i])):
-            # Check if the current element is equal to "H".
-            if default_spot[i][j] == 'H':
-                # Print the position of "H".
-                return print(f"Found 'H' at position ({i},{j}).")
-
-
-if __name__ == "__main__":
-    
-    my_input = read_file()
-    print(my_input)
-    print()    
-    default_spot = original_board()
-    print(default_spot)
     print()
-    find_moves()
+
+
+    # Iterate over each row.
+    for i in range(len(my_list)):
+        # Iterate over each column.
+        for j in range(len(my_list[i])):
+            # Check if the current element is equal to "H".
+            if my_list[i][j] == 'H':
+                # Print the position of "H".
+                print(f"Found 'H' at position ({i},{j}).")
+                # Print new index and new position of "H".
+                h_row = my_list[i]
+                print(h_row)
+                h_column_index = h_row.index('H')
+                print(h_column_index)
+                h_column_index_new = h_column_index + 4
+                print(h_column_index_new)
+                my_list[i][j] = '.'
+                my_list[i][h_column_index_new] = 'H'
+                print(h_row)
+                break
