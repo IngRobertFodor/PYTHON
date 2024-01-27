214c3d17be49ce70044444bc0aaca99202c292cd
Robert Fodor <ingrobertfodor@gmail.com>
2024-01-28 00:07:19 +0100

    Update

--- pythonprogrammingexercises/exercise_10_find_and_replace_for_loop.py
+++ pythonprogrammingexercises/exercise_10_find_and_replace_for_loop.py
@@ -1,7 +1,9 @@
 # Find and Replace using For loop
 
 def find_and_replace(my_sentence, old_text, new_text):
+    
     print("Original sentence was:", my_sentence)
+    
     for i in range(0, len(my_sentence)):
         if my_sentence[i:i + len(old_text)] == old_text:
             print("Old text was:", old_text)
---
+++ pythonprogrammingexercises/exercise_10_find_and_replace_while_loop.py
@@ -0,0 +1,15 @@
+# Find and Replace using While loop
+
+def find_and_replace(my_sentence, old_text, new_text):
+    
+    i=0
+    while True:
+        if my_sentence[i:i + len(old_text)] == old_text:
+            new_sentence = my_sentence.replace(my_sentence[i:i + len(old_text)], new_text)          
+            return print(new_sentence)
+        else:
+            i+=1        
+        
+
+find_and_replace("This is her dog, his name is Doggy.", "her", "our")
+find_and_replace("This is her dog, his name is Doggy.", "Doggy", "DOGGY")
