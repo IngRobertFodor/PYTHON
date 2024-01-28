f0aff61c789e75a34307eb9139da8633fc955ced
Robert Fodor <ingrobertfodor@gmail.com>
2024-01-28 23:11:58 +0100

    Update

--- pythonprogrammingexercises/exercise_6_ordinal_suffix.py
+++ pythonprogrammingexercises/exercise_6_ordinal_suffix.py
@@ -6,12 +6,14 @@ import random
 def ordinal_suffix(number):
 
     number=str(number)
-    print(number)
-    number_list = list(number)
-    last_num = number_list[-1]
-    print("This letter decide about the suffix: " + str(last_num))
-
-    if int(last_num) == 0:
+    last_num = number[-1:]
+    last_two_nums = number[-2:]
+    
+    if int(last_two_nums) in range(11,14):
+        suffix = "th"
+        result = number + suffix
+        return print(result)
+    elif int(last_num) == 0:
         suffix = "th"
         result = number + suffix
         return print(result)
@@ -29,18 +31,31 @@ def ordinal_suffix(number):
         return print(result)
     elif int(last_num) in range (4,10):
         suffix = "th"
-        result = number + suffix      
+        result = number + suffix
         return print(result)
         
 
-ordinal_suffix(80)
+ordinal_suffix(0)
+ordinal_suffix(1)
+ordinal_suffix(2)
+ordinal_suffix(3)
+ordinal_suffix(4)
+ordinal_suffix(5)
+ordinal_suffix(6)
+ordinal_suffix(7)
+ordinal_suffix(8)
+ordinal_suffix(9)
+ordinal_suffix(10)
+ordinal_suffix(11)
+ordinal_suffix(12)
+ordinal_suffix(13)
+ordinal_suffix(14)
+ordinal_suffix(15)
 ordinal_suffix(21)
 ordinal_suffix(22)
 ordinal_suffix(23)
 ordinal_suffix(24)
 ordinal_suffix(25)
-ordinal_suffix(26)
-ordinal_suffix(27)
-ordinal_suffix(28)
-ordinal_suffix(29)
-ordinal_suffix(50)
+ordinal_suffix(30)
+ordinal_suffix(101)
+ordinal_suffix(80)
