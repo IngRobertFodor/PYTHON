852a27aea8e94eedbc5d34aa1fcac4d0166d4e87
Robert Fodor <ingrobertfodor@gmail.com>
2024-01-29 20:10:22 +0100

    Update

--- pythonprogrammingexercises/exercise_11_hours_minutes_seconds.py
+++ pythonprogrammingexercises/exercise_11_hours_minutes_seconds.py
@@ -1,32 +1,34 @@
 # Hours, Minutes, Seconds
 
 def get_hours_minutes_seconds(original_seconds):
-    raw_hours = original_seconds / 3600
-    #print(raw_hours)
-    #print("Whole Hours:", int(raw_hours))
-    hours = int(raw_hours)
-    my_hours = str(hours) + "h"
-    #print(my_hours)
-
-    raw_minutes = (raw_hours - int(raw_hours)) * 60
-    #print(raw_minutes)
-    #print("Whole Minutes", int(raw_minutes))
-    minutes = int(raw_minutes)
-    my_minutes = str(minutes) + "m"
-    #print(my_minutes)
-
-    raw_seconds = (raw_minutes - int(raw_minutes)) * 60
-    #print(raw_seconds)
-    #print("Whole Seconds", int(raw_seconds))
-    seconds = round(raw_seconds)
-    my_seconds = str(seconds) + "s"
-    #print(my_seconds)
-
-    result = my_hours + " " + my_minutes + " " + my_seconds
-
+    
     if original_seconds == 0:
         result = "0s"
-
+    
+    raw_hours = original_seconds / 3600
+    hours = int(raw_hours)
+    days = ""
+    if hours >= 24:
+        days = hours // 24
+        my_days = str(days) + "d"
+        my_hours = hours % 24
+        my_hours = str(my_hours) + "h"
+    else:
+        my_hours = str(hours) + "h"
+    
+    raw_minutes = (raw_hours - int(raw_hours)) * 60
+    minutes = int(raw_minutes)
+    my_minutes = str(minutes) + "m"
+    
+    raw_seconds = (raw_minutes - int(raw_minutes)) * 60
+    seconds = round(raw_seconds)
+    my_seconds = str(seconds) + "s"
+    
+    if (hours//24) < 1:
+        result = my_hours + " " + my_minutes + " " + my_seconds
+    else:
+        result = my_days + " " + my_hours + " " + my_minutes + " " + my_seconds
+    
     return print(result)
 
 
@@ -38,3 +40,4 @@ get_hours_minutes_seconds(3600)
 get_hours_minutes_seconds(3601)
 get_hours_minutes_seconds(3661)
 get_hours_minutes_seconds(90042)
+get_hours_minutes_seconds(12345678)
