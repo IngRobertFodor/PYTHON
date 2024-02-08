b887e1f6c245f21138745a5a9279d3ea0a7fe25c
Robert Fodor <ingrobertfodor@gmail.com>
2024-02-08 19:33:34 +0100

    Update Script4

--- pythonprogrammingexercises/exercise_4_area_volume.py
+++ pythonprogrammingexercises/exercise_4_area_volume.py
@@ -2,26 +2,37 @@
 
 def area(length, width):
     area = length * width
-    return print(str(area) + "cm2")
+    return area
 
 def perimeter(length, width):
     perimeter = length + width + length + width
-    return print(str(perimeter) + "cm")
+    return perimeter
 
 def volume(length, width, height):
     volume = length * width * height
-    return print(str(volume) + "cm3")
+    return volume
 
 def surface_area(length, width, height):
     surface_area = (length * width * 2) + (length * height * 2) + (width * height * 2)
-    return print(str(surface_area) + "cm2")
+    return surface_area
 
 
-area(10, 8)
-area(1, 2)
-perimeter(10, 8)
-perimeter(1, 2)
-volume(10, 8, 5)
-volume(1, 2, 3)
-surface_area(10, 8, 5)
-surface_area(1, 2, 3)
+result_area_one = area(10, 8)
+print(str(result_area_one) + "cm2")
+result_area_two = area(1, 2)
+print(str(result_area_two) + "cm2")
+
+result_perimeter_one = perimeter(10, 8)
+print(str(result_perimeter_one) + "cm")
+result_perimeter_two = perimeter(1, 2)
+print(str(result_perimeter_two) + "cm")
+
+result_volume_one = volume(10, 8, 5)
+print(str(result_volume_one) + "cm3")
+result_volume_two = volume(1, 2, 3)
+print(str(result_volume_two) + "cm3")
+
+result_surface_area_one = surface_area(10, 8, 5)
+print(str(result_surface_area_one) + "cm2")
+result_surface_area_two = surface_area(1, 2, 3)
+print(str(result_surface_area_two) + "cm2")
