999bb1f1997cde7a0a716f7209977fbb802f3cda
Robert Fodor <ingrobertfodor@gmail.com>
2024-02-08 19:52:01 +0100

    Update Script5

--- pythonprogrammingexercises/exercise_5_fizz_buzz.py
+++ pythonprogrammingexercises/exercise_5_fizz_buzz.py
@@ -5,19 +5,20 @@ up_to = int(up_to)
 
 def fizz_buzz(up_to):    
     range_numbers = range(1, up_to + 1)
+    result_list = []
     for number in range_numbers:
         if number % 3 == 0 and number % 5 == 0:
-            number = "FizzBuzz"
+            result_list.append("FizzBuzz")
         elif number % 3 == 0:
-            number = "Fizz"
+            result_list.append("Fizz")
         elif number % 5 == 0:
-            number = "Buzz"   
+            result_list.append("Buzz")   
         else:
         # number % 3 != 0
         # number % 5 != 0
-            number = number
-        print(number, "", end="")
-    return
+            result_list.append(number)
+    return result_list
 
 
-fizz_buzz(up_to)
+result_list_up_to = fizz_buzz(up_to)
+print(result_list_up_to)
