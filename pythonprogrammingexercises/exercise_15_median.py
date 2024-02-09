0018a3e9ebe6d5c7ebd91ae3b8adf124e8eaf5e7
Robert Fodor <ingrobertfodor@gmail.com>
2024-02-09 11:40:43 +0100

    Add Asserts

--- pythonprogrammingexercises/exercise_15_median.py
+++ pythonprogrammingexercises/exercise_15_median.py
@@ -7,7 +7,7 @@ my_list_two = [3, 7, 10, 4, 1, 6, 9, 5, 2, 8]
 def median(my_list):
     
     my_list.sort()
-    print(my_list)
+    #print(my_list)
     if len(my_list) == 0:
         return None
     elif len(my_list) % 2 != 0:
@@ -20,6 +20,20 @@ def median(my_list):
         x_two = my_list[x-1]
         median = (x_one + x_two) / 2
         return median
+
+
+# Asserts
+assert median([]) == None
+assert median([1, 2, 3]) == 2
+assert median([3, 7, 10, 4, 1, 9, 6, 5, 2, 8]) == 5.5
+assert median([3, 7, 10, 4, 1, 9, 6, 2, 8]) == 6
+import random 
+random.seed(42) 
+testData = [3, 7, 10, 4, 1, 9, 6, 2, 8] 
+for i in range(1000): 
+    random.shuffle(testData) 
+    assert median(testData) == 6
+print()
 
 
 result_one = median(my_list_empty)
