a7109c41de271ebd0139e8209129f8a6a2fce4f4
Robert Fodor <ingrobertfodor@gmail.com>
2024-02-27 11:36:18 +0100

    Update

--- pythonprogrammingexercises/exercise_25_multiplication_table.py
+++ pythonprogrammingexercises/exercise_25_multiplication_table.py
@@ -1,6 +1,19 @@
 # Multiplication Table
 
-for row in range(1,11):
+special_character = "  |"
+print(special_character.rjust(2), end="")
+
+top_line = ["1","2","3","4","5","6","7","8","9","10"]
+for i in top_line:
+    print(i.rjust(2), " ", end="")
+print()
+
+print("--+---------------------------------------")
+
+for row in range(1,11):   
+    str_row = str(row)
+    print(str_row.rjust(2), end="")
+    print("|", end="")
     for column in range(1,11):
         xy = row*column
         xy = str(xy)
