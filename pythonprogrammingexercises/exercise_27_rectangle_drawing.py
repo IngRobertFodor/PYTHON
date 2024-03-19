08a2939ba1540c693a95bd3802b95d80e4a26dcc
Robert Fodor <ingrobertfodor@gmail.com>
2024-03-19 15:15:59 +0100

    Script27

---
+++ pythonprogrammingexercises/exercise_27_rectangle_drawing.py
@@ -0,0 +1,19 @@
+# Rectangle Drawing
+
+def draw_rectangle(width,height):
+    if type(height)!=int or height<=0 or type(width)!=int or width<=0:
+        return print("None")
+    else:
+        for row in range(0,height):
+            for hashtag in range(0,width):
+                print("#", end="")
+            print()
+        return
+
+
+draw_rectangle(10,4)
+print()
+draw_rectangle(-10,4)
+draw_rectangle(10,-4)
+draw_rectangle(10,0)
+draw_rectangle(0,10)
