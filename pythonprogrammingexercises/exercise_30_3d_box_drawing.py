5487260ffddb7fa280bbd3da6b6aed2583ed65d9
Robert Fodor <ingrobertfodor@gmail.com>
2024-03-22 13:58:00 +0100

    Update

--- pythonprogrammingexercises/exercise_30_3d_box_drawing.py
+++ pythonprogrammingexercises/exercise_30_3d_box_drawing.py
@@ -6,23 +6,29 @@ def draw_3d_box(size):
     
     else:
         
-        print(" "*(size), "+", ((size)*2)*"-", "+")
+        print(" "*(size+1), "+", (size*2)*"-", "+", sep="")
         
         for i in range(0,size):
-            print(" "*(size-(i+1)), "/", ((size)*2)*" ", "/")
+            print(" "*(size-i), "/", (size*2)*" ", "/", sep="", end="")
+            print(" "*(i), "|", sep="")
+      
+        print("+", (size*2)*"-", "+", sep="", end="")
+        print(size*" ", "+", sep="")
         
-        print("+", ((size)*2)*"-", "+")
+        for n in range(0,size):
+            print("|", (size*2)*" ", "|", sep="")
         
-        for j in range(0,size):
-            print("|", ((size)*2)*" ", "|")
-        
-        print("+", ((size)*2)*"-", "+")
+        print("+", (size*2)*"-", "+", sep="")
 
 
+'''
 draw_3d_box(1)
 draw_3d_box(2)
+'''
 draw_3d_box(3)
+'''
 draw_3d_box(4)
 draw_3d_box(5)
 print()
 draw_3d_box(0)
+'''
