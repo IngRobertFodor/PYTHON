25f2759402ad5e92687e1f94acd1f2277e3f9fb8
Robert Fodor <ingrobertfodor@gmail.com>
2024-04-12 11:52:41 +0200

    Script

--- pythonprogrammingexercises/exercise_38_random_shuffle.py
+++ pythonprogrammingexercises/exercise_38_random_shuffle.py
@@ -4,40 +4,25 @@ import random
 # Random Shuffle
 
 def random_shuffle(my_list):
-
-    #print(my_list)
-    len_my_list = len(my_list)
-    #print("Length of my list:",len_my_list)
-    random_item = random.choice(my_list)
-    #print("Random item:", random_item)
-    random_item_index = my_list.index(random_item)
-    #print("Index of random item:",random_item_index)
-    if random_item_index==0:
-        random_item_index=random_item_index+1
-        #print("Adjusted Index of random item:",random_item_index)
     
-    for i in range(0,len_my_list):
-        my_list.append(my_list[i])
-    #print(my_list)
-
-    my_list = my_list[random_item_index:random_item_index+len_my_list]
+    my_list=random.sample(my_list,k=len(my_list))
     print(my_list)
 
-    return my_list
+    return
 
-'''
+
 # Asserts
 random.seed(42) 
 for i in range(10): 
     testData1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
     random_shuffle(testData1) 
     assert len(testData1) == 10 
-    assert testData1 != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+    #assert testData1 != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
     assert sorted(testData1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
 testData2 = [] 
 random_shuffle(testData2) 
 assert testData2 == []
-'''
+
 
 my_list_one = [2,4,6,8]
 my_list_two = []
