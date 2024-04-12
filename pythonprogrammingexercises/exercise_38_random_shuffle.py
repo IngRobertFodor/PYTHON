0e73d37def6cf1efe85d9422be6f49ed2199510f
Robert Fodor <ingrobertfodor@gmail.com>
2024-04-12 11:57:45 +0200

    Script 38 Random Shuffle - Final Version

--- pythonprogrammingexercises/exercise_38_random_shuffle.py
+++ pythonprogrammingexercises/exercise_38_random_shuffle.py
@@ -8,16 +8,16 @@ def random_shuffle(my_list):
     my_list=random.sample(my_list,k=len(my_list))
     print(my_list)
 
-    return
+    return my_list
 
 
 # Asserts
 random.seed(42) 
 for i in range(10): 
     testData1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
-    random_shuffle(testData1) 
+    testData1 = random_shuffle(testData1) 
     assert len(testData1) == 10 
-    #assert testData1 != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+    assert testData1 != [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
     assert sorted(testData1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
 testData2 = [] 
 random_shuffle(testData2) 
