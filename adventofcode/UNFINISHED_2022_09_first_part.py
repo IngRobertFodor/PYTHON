7271dfbcb29b1cf63c18c520162383fb0f49460d
Robert Fodor <ingrobertfodor@gmail.com>
2024-05-07 21:18:49 +0200

    WIP

--- adventofcode/UNFINISHED_2022_09_first_part.py
+++ adventofcode/UNFINISHED_2022_09_first_part.py
@@ -1,52 +1,22 @@
 #1 Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?
 
-'''
-# Training
-#########################################################################################
-result_one = []
-result_two = []
-list_aaaaa = ["Test_a","Test_aa","Test_bb","Test_aaa","Test_aaaa"]
-list_bbbbb = ["Test_b","Test_bb","Test_bbb","Test_bbbb","Test_aa"]
-list_ccccc = ["Test_c","Test_cc","Test_aa","Test_ccc","Test_cccc"]
-for i in list_aaaaa:
-    for j in list_bbbbb:
-        if i == j:
-            result_one.append(i)
-            for k in list_ccccc:
-                if i == k:
-                    result_two.append(i)
-print(result_one)
-print(result_two)
-print()
-#########################################################################################
-'''
 
-my_list = [
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']    
-    ]
+my_list = list()
+for i in range(900):
+    i='.'
+    my_list.append(i)
+print(my_list)
+print()
+
+my_list_chunked = []
+chunk_size = 30
+for i in range(0, len(my_list), chunk_size):
+    my_list_chunked.append(my_list[i:i+chunk_size])
+print(my_list_chunked)
+
 start = "s"
-my_list[19][0] = start
-for item in my_list:
-    print(item)
+my_list_chunked[19][0] = start
+print(my_list_chunked[19][0])
 print()
 
 t_movement_row = 19
@@ -59,31 +29,12 @@ dot_place = "."
 
 my_dictionary = {"my_direction": "", "steps": ""}
 
-my_list_tail_positions = [
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']    
-    ]
-my_list_tail_positions [19][0] = start
+my_list_chunked_tail_positions = my_list_chunked.copy()
+print(my_list_chunked_tail_positions)
+my_list_chunked_tail_positions[19][0] = start
+print(my_list_chunked_tail_positions[19][0])
 
-
+'''
 with open("UNFINISHED_2022_09.txt", "r") as open_file:  
 
     lines = open_file.readlines()
@@ -101,35 +52,35 @@ with open("UNFINISHED_2022_09.txt", "r") as open_file:
             print(h_movement_row,h_movement_column)
             # Head Moves
             for move in range(0,int(my_dictionary["steps"])):
-                my_list[h_movement_row][h_movement_column] = dot_place
+                my_list_chunked[h_movement_row][h_movement_column] = dot_place
                 h_movement_column = h_movement_column + 1
                 print("h moves:",h_movement_row,h_movement_column)
-                my_list[h_movement_row][h_movement_column] = head_place
-                my_list[t_movement_row][t_movement_column] = tail_place
-                for item in my_list:
-                    print(item)
+                my_list_chunked[h_movement_row][h_movement_column] = head_place
+                my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                #for item in my_list_chunked:
+                #    print(item)
                 # Tail Moves
-                my_list[t_movement_row][t_movement_column] = tail_place
+                my_list_chunked[t_movement_row][t_movement_column] = tail_place
                 if h_movement_row != t_movement_row and h_movement_column !=t_movement_column and h_movement_column-t_movement_column==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_row = h_movement_row
                     t_movement_column = h_movement_column - 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                #    print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)
                 elif h_movement_column-t_movement_column==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_column = h_movement_column - 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)    
+                #    print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)    
             print()
-            print("Result List:",t_movement_row,t_movement_column)
-            print()
+            #print("Result List:",t_movement_row,t_movement_column)
+            #print()
 
         ### This should have all conditions implemented (It should be similar in all directions).        
         elif my_dictionary["my_direction"] == "U":
@@ -137,106 +88,107 @@ with open("UNFINISHED_2022_09.txt", "r") as open_file:
             ## Head Moves
             # This is the "H" movement.
             for move in range(0,int(my_dictionary["steps"])):
-                my_list[h_movement_row][h_movement_column] = dot_place
+                my_list_chunked[h_movement_row][h_movement_column] = dot_place
                 h_movement_row = h_movement_row - 1
                 print("h moves:",h_movement_row,h_movement_column)
-                my_list[h_movement_row][h_movement_column] = head_place
-                for item in my_list:
-                    print(item)
+                my_list_chunked[h_movement_row][h_movement_column] = head_place
+                #for item in my_list_chunked:
+                #    print(item)
                 ## Tail Moves
                 # This alligns rows and columns for "H" and "T".
                 if h_movement_row != t_movement_row and h_movement_column !=t_movement_column and t_movement_row-h_movement_row==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_column = h_movement_column
                     t_movement_row = h_movement_row + 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                #    print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                #    for item in my_list_chunked:
+                #        print(item)
                 # This will continue in following the "H" with "T".
                 elif t_movement_row-h_movement_row==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_row = h_movement_row + 1
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    #print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)
             print()
-            print("Result List:",t_movement_row,t_movement_column)
-            print()
+            #print("Result List:",t_movement_row,t_movement_column)
+            #print()
 
         elif my_dictionary["my_direction"] == "L":
             print(h_movement_row,h_movement_column)
             # Head Moves
             for move in range(0,int(my_dictionary["steps"])):
-                my_list[h_movement_row][h_movement_column] = dot_place
+                my_list_chunked[h_movement_row][h_movement_column] = dot_place
                 h_movement_column = h_movement_column - 1
                 print("h moves:",h_movement_row,h_movement_column)
-                my_list[h_movement_row][h_movement_column] = head_place
-                for item in my_list:
-                    print(item)
+                my_list_chunked[h_movement_row][h_movement_column] = head_place
+                #for item in my_list_chunked:
+                #    print(item)
                 # Tail Moves
-                my_list[t_movement_row][t_movement_column] = tail_place
+                my_list_chunked[t_movement_row][t_movement_column] = tail_place
                 if h_movement_row != t_movement_row and h_movement_column !=t_movement_column and t_movement_column-h_movement_column==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_row = h_movement_row
                     t_movement_column = h_movement_column + 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                    #print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)
                 elif t_movement_column-h_movement_column==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_column = h_movement_column + 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                    #print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)
             print()
-            print("Result List:",t_movement_row,t_movement_column)
-            print()
+            #print("Result List:",t_movement_row,t_movement_column)
+            #print()
 
         elif my_dictionary["my_direction"] == "D":
             print(h_movement_row,h_movement_column)
             # Head Moves
             for move in range(0,int(my_dictionary["steps"])):
-                my_list[h_movement_row][h_movement_column] = dot_place
+                my_list_chunked[h_movement_row][h_movement_column] = dot_place
                 h_movement_row = h_movement_row + 1
                 print("h moves:",h_movement_row,h_movement_column)
-                my_list[h_movement_row][h_movement_column] = head_place
-                for item in my_list:
-                    print(item)
+                my_list_chunked[h_movement_row][h_movement_column] = head_place
+                #for item in my_list_chunked:
+                #    print(item)
                 # Tail Moves
-                my_list[t_movement_row][t_movement_column] = tail_place
+                my_list_chunked[t_movement_row][t_movement_column] = tail_place
                 if h_movement_row != t_movement_row and h_movement_column !=t_movement_column and h_movement_row-t_movement_row==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_column = h_movement_column
                     t_movement_row = h_movement_row - 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                    #print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)
                 elif h_movement_row-t_movement_row==2:
-                    my_list[t_movement_row][t_movement_column] = dot_place
+                    my_list_chunked[t_movement_row][t_movement_column] = dot_place
                     t_movement_row = h_movement_row - 1
-                    print("t moves:",t_movement_row,t_movement_column)
-                    my_list[t_movement_row][t_movement_column] = tail_place
-                    my_list_tail_positions[t_movement_row][t_movement_column] = "#"
-                    for item in my_list:
-                        print(item)
+                    #print("t moves:",t_movement_row,t_movement_column)
+                    my_list_chunked[t_movement_row][t_movement_column] = tail_place
+                    my_list_chunked_tail_positions[t_movement_row][t_movement_column] = "#"
+                    #for item in my_list_chunked:
+                    #    print(item)
             print()
-            print("Result List:",t_movement_row,t_movement_column)
-            print()
+            #print("Result List:",t_movement_row,t_movement_column)
+            #print()
     
 
 ###
 print("This is result list.")
-for item in my_list_tail_positions:
+for item in my_list_chunked_tail_positions:
     print(item)
 print()
+'''
