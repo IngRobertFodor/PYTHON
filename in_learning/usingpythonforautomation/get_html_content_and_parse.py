d6d702d24ca3e8077cbef4a9c1aa93fa768ff197
Robert Fodor <ingrobertfodor@gmail.com>
2024-07-19 15:55:09 +0200

    Web Automation Part1

---
+++ in_learning/usingpythonforautomation/get_html_content_and_parse.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://market.feedbooks.com/"
+
+html_content = requests.get(url).text
+print(html_content)
+
+parsed_html = BeautifulSoup(html_content, "html.parser")
+print(parsed_html)
