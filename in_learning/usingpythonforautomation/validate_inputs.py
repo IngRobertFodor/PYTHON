bbca1592bfb05934c8aa4f63ce6ae3bdbfe84602
Robert Fodor <ingrobertfodor@gmail.com>
2024-07-19 10:06:27 +0200

    Update

--- in_learning/usingpythonforautomation/validate_inputs.py
+++ in_learning/usingpythonforautomation/validate_inputs.py
@@ -7,8 +7,11 @@ print("\nEXAMPLE 1")
 result = pyip.inputInt("Enter the number of shopping bags you will need for your items:", min=0)
 print("\nYou will use", result, "store bags.")
 
-print("\nEXAMPLE 2")
+print("\nEXAMPLE 2.1")
 result = pyip.inputMenu(["red", "blue", "green"], lettered=True, numbered=False)
+print("\nYou have chosen a", result, "marker.")
+print("\nEXAMPLE 2.2")
+result = pyip.inputMenu(["red", "blue", "green"], lettered=False, numbered=True)
 print("\nYou have chosen a", result, "marker.")
 
 print("\nEXAMPLE 3")
