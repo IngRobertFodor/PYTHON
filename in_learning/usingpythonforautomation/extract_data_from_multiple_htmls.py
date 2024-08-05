f8bd8cc72f99f4b837e436e13f45d414cf3183a9
Robert Fodor <ingrobertfodor@gmail.com>
2024-08-05 12:39:41 +0200

    Script Update

--- in_learning/usingpythonforautomation/extract_data_from_multiple_htmls.py
+++ in_learning/usingpythonforautomation/extract_data_from_multiple_htmls.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
 
+'''
 url = "https://market.feedbooks.com/"
 html_content = requests.get(url).text
 parsed_html = BeautifulSoup(html_content, "html.parser")
@@ -14,3 +16,18 @@ for author in bookauthors:
     bookauthors_list.append(author.text.strip())
 print()
 print("These are bookauthors on web page:", bookauthors_list)
+'''
+
+
+for page_number in range(2, 5):
+    url_martinus_klasics_allpages = "https://www.martinus.sk/l?categories%5B0%5D=6100&clasmods%5B0%5D=308346&page="+str(page_number)
+    html_content_martinus = requests.get(url_martinus_klasics_allpages).text
+    parsed_html_martinus = BeautifulSoup(html_content_martinus, "html.parser")
+    #print(parsed_html_martinus.prettify())
+    booktitles_martinus = parsed_html_martinus.find_all("span", class_="link link--product")
+    #print(booktitles_martinus)
+    print("Page:", page_number)
+    for book_title in booktitles_martinus:
+        print(book_title.text.strip())
+    print()
+    sleep(1)
