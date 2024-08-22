48ecf34ed0e827c99f12f8b6d98dc32d92893ed9
Robert Fodor <ingrobertfodor@gmail.com>
2024-08-22 15:43:15 +0200

    Web Scraping with Python (inLearning) - Basicss

--- in_learning/web_scraping_with_python/building_web_scraper.py
+++ in_learning/web_scraping_with_python/building_web_scraper.py
@@ -14,17 +14,16 @@
 
 # STEP 2:
 # Create a new Scrapy project.
-# Test web page: https://pythonscraping.com/linkedin/ietf.html
+# Test web page: https://pythonscraping.com/linkedin/ietf.html      (Other example: https://en.wikipedia.org.)
 
-## scrapy startproject ietf_scraper
+## scrapy startproject ietf_scraper     (Other example: scrapy startproject wiki_scraper.)
 
 
 # STEP 3:
 # Move to the spiders directory and create new spider.
 
-## cd ietf_scraper/ietf_scraper/spiders
-## scrapy genspider pythonscraping pythonscraping.com
-
+## cd ietf_scraper/ietf_scraper/spiders     (Other example: cd wiki_scraper/wiki_scraper/spiders.)
+## scrapy genspider pythonscraping pythonscraping.com       (Other example: scrapy genspider wikiscraping en.wikipedia.org.)
 
 # STEP 4:
 # Edit the spider file to extract the data.
@@ -33,4 +32,4 @@
 # STEP 5:
 # Run the spider.
 
-## scrapy runspider pythonscraping.py
+## scrapy runspider pythonscraping.py       (Other example: scrapy runspider wikiscraping.py.)
---
+++ in_learning/web_scraping_with_python/wiki_scraper/wiki_scraper/spiders/wikiscraping.py
@@ -0,0 +1,29 @@
+# CRAWLSPIDER
+
+
+import scrapy # type: ignore
+from scrapy.spiders import CrawlSpider, Rule # type: ignore
+from scrapy.linkextractors import LinkExtractor # type: ignore
+# CRAWLSPIDER
+from wiki_scraper.items import WikiScraperItem # type: ignore
+
+
+class WikiscrapingSpider(CrawlSpider):
+    name = "wikiscraping"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Kevin_Bacon"]
+
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback="parse_info", follow=True)]
+    
+    def parse_info(self, response):
+        
+        wikicrawler = WikiScraperItem()
+
+        wikicrawler['title'] = response.xpath("//h1/i/text()").get() 
+        wikicrawler['url'] = response.url                               
+        
+        return wikicrawler
+
+
+# CRAWLSPIDER
+## scrapy runspider wikiscraping.py -o articles.xml -t xml -s CLOSESPIDER_ITEMCOUNT=10
