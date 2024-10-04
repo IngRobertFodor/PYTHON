629188ba427dbed1ad911992a68d1260058fcadc
Robert Fodor <ingrobertfodor@gmail.com>
2024-10-04 23:08:15 +0200

    LinkedIn Learning - Python LevelUp - Exercise 11

--- linkedinlearning/leveluppython/generate_a_password.py
+++ linkedinlearning/leveluppython/generate_a_password.py
@@ -1,19 +1,35 @@
 import random
 
 
-def generate_special_password(num):
+def generate_special_password(number_representing_length_of_password):
 
     dice_num_list = []
-    for i in range(num):
-        i = random.randint(1, 7)
-        dice_num_list.append(str(i))
-    print(dice_num_list)
-    dice_num = "".join(dice_num_list)
-    print(dice_num)
+    generated_password = []
+    for i in range(number_representing_length_of_password):
+        for j in range(5):
+            j = random.randrange(1, 7)
+            dice_num_list.append(str(j))
+        print(dice_num_list)
+        dice_num = "".join(dice_num_list)
+        print(dice_num)
+        dice_num_list.clear()
+        
+        password_file = open("dicewarewordlist.txt", "r")
+        for line in password_file:
+            #print(line)
+            line_list = list(line.split())
+            #print(line_list)
+            if len(line_list)==2 and dice_num == line_list[0]:
+                print(line_list[1])
+                generated_password.append(line_list[1])
+                break
+    print(generated_password)
+    generated_password = " ".join(generated_password)
+    print()
 
-    password_file = open("dicewarewordlist.pdf", "r")
+    password_file.close()
     
-    return
+    return print("Password generated successfully:", generated_password)
 
 
 generate_special_password(5)
