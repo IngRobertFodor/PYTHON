95fb9be5a617f31822fbd55506e4a7a7a65957eb
Robert Fodor <ingrobertfodor@gmail.com>
2024-10-16 13:58:55 +0200

    Update Script

--- linkedinlearning/pythonobjectorientedprogramming/basic_class_definition.py
+++ linkedinlearning/pythonobjectorientedprogramming/basic_class_definition.py
@@ -15,16 +15,44 @@ class Book:
 book_one = Book("My Book", 5.12345, 0.2)
 book_two = Book(title="My Second Book", price=20.00, discount=0.5)
 
+# This will print the type of the book_one.
+# Output: <class '__main__.Book'>.
+print("The type of book_one is:", type(book_one))
 
-# This will print the title of the book one.
-print("This is title of book one:", book_one.title)
-# This will print the price of the book one.
-print("This is price of book one:", book_one.get_price())
+# This will check if book_one is an instance of Book class.
+# Output: True.
+print("Is book_one an instance of Book class?", isinstance(book_one, Book))
+
+# This will print the title of the book_one.
+print("This is title of book_one:", book_one.title)
+# This will print the price of the book_one.
+print("This is price of book_one:", book_one.get_price())
+
 ### ROUNDING DECIMALS
-# This will print the price of the book one rounded to 4 and 5 decimals.
-print("The book one price rounded to 4 decimals:", round(book_one.get_price(), 4))
-print("The book one price rounded to 5 decimals:", "{:.5f}".format(book_one.get_price()))
-# This will print the discounted price of the book one.
-print("This is discounted price of book one:", book_one.get_discounted_price())
-# This will print the title of the book two.
-print("This is title of book two:", book_two.title)
+# This will print the price of the book_one rounded to 4 and 5 decimals.
+print("The book_one price rounded to 4 decimals:", round(book_one.get_price(), 4))
+print("The book_one price rounded to 5 decimals:", "{:.5f}".format(book_one.get_price()))
+
+# This will print the title of the book_two.
+print("This is title of book_two:", book_two.title)
+print()
+
+
+class Newspaper():
+    def __init__(self, title):
+        self.title = title
+
+
+newspaper_one = Newspaper("The New York Times")
+
+# This will print the type of the newspaper_one.
+# Output: <class '__main__.Newspaper'>.
+print("The type of newspaper_one is:", type(newspaper_one))
+
+# This will check if newspaper_one is an instance of Book class.
+# Output: False.
+print("Is newspaper_one an instance of Book class?", isinstance(newspaper_one, Book))
+
+# This will print the title of the newspaper_one.
+print("This is title of newspaper_one:", newspaper_one.title)
+print()
