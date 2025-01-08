b679e1d744f81bc6fe2a3882ffd7240c8a3a126e
Robert Fodor <ingrobertfodor@gmail.com>
2025-01-08 16:41:47 +0100

    Update Scripts

--- linkedinlearning/pythonautomationandtesting/locate_web_elements.py
+++ linkedinlearning/pythonautomationandtesting/locate_web_elements.py
@@ -1,42 +1,32 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 driver = webdriver.Firefox()
 driver.get("https://www.selenium.dev/")
+print("Page Title: ", driver.title)
 time.sleep(2)
-print()
 
-title_relative_path = driver.find_element(By.XPATH, "//h1")
-print("Find element by XPATH:",title_relative_path.text)
+title_relative_xpath = driver.find_element(By.XPATH, "//h1")
+print("Find element by XPATH:",title_relative_xpath.text)
 
-title_relative_path_two = driver.find_element(By.XPATH, "//h1[@class='d-1 fw-bold']")
-print("Find element by XPATH:",title_relative_path_two.text)
+title_relative_xpath_two = driver.find_element(By.XPATH, "//h1[@class='d-1 fw-bold']")
+print("Find element by XPATH:",title_relative_xpath_two.text)
 
-title_relative_path_id = driver.find_element(By.TAG_NAME, "h1")
-print("Find element by TAG_NAME:",title_relative_path_id.text)
+title_relative_xpath_three = driver.find_element(By.XPATH, "/html/body/div/main/section[1]/div/div/div/h1")
+print("Find element by XPATH:",title_relative_xpath_three.text)
+
+title_relative_path_tag_name = driver.find_element(By.TAG_NAME, "h1")
+print("Find element by TAG_NAME:",title_relative_path_tag_name.text)
 
 selenium_icon = driver.find_element(By.CLASS_NAME, "navbar-logo")
 print("Find element by CLASS_NAME:", selenium_icon.text)
 
-selenium_ide_icon = driver.find_element(By.CLASS_NAME, "selenium-button-container")
-print("Find element by CLASS_NAME:", selenium_ide_icon.text)
+selenium_ide_icon = driver.find_element(By.ID, "selenium_ide")
+print("Find element by ID:", selenium_ide_icon.text)
 
-print()
-
-exercise_element_by_id = driver.find_element(By.ID, "selenium_webdriver")
-print("Find element by ID:", exercise_element_by_id)
-print("Find element by ID:", exercise_element_by_id.text)
-
-exercise_element_by_tag_name = driver.find_element(By.TAG_NAME, "button")
-print("Find element by TAG NAME:", exercise_element_by_tag_name)
-
-exercise_element_by_xpath = driver.find_element(By.XPATH, "//h1[@class='d-1 fw-bold']")
-print("Find element by XPATH:", exercise_element_by_xpath.text)
-
-exercise_element_by_class_name = driver.find_element(By.CLASS_NAME, "selenium-logo")
-print("Find element by CLASS_NAME:", exercise_element_by_class_name)
+text_relative_xpath = driver.find_element(By.XPATH, "//p[@class='lead mt-3 mb-0']")
+print("Find element by XPATH:",text_relative_xpath.text)
 
 driver.quit()
--- linkedinlearning/pythonautomationandtesting/web_elements_drag_and_drop.py
+++ linkedinlearning/pythonautomationandtesting/web_elements_drag_and_drop.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
-# Interaction with the elements.
-from selenium.webdriver.common.keys import Keys
-# DROPDOWNs
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 # DRAG AND DROPs
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -12,7 +8,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Firefox()
 driver.get("https://the-internet.herokuapp.com/drag_and_drop")
 time.sleep(2)
-
 
 # DRAG AND DROP element.
 source_element = driver.find_element(By.XPATH, "//*[@id='column-a']")
@@ -24,6 +19,5 @@ time.sleep(4)
 
 source_element = driver.find_element(By.XPATH, "//*[@id='column-b']")
 print("B:",source_element.text)
-
 
 driver.quit()
