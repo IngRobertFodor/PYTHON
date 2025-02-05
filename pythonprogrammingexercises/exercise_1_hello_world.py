2ca896ff5b24d5fbf323d8ba00df845e07b26a6d
Robert Fodor <ingrobertfodor@gmail.com>
2025-02-05 14:08:07 +0100

    Updates

--- pythonprogrammingexercises/exercise_1_hello_world.py
+++ pythonprogrammingexercises/exercise_1_hello_world.py
@@ -2,4 +2,4 @@
 
 print("Hello world!")
 name = input("What is your name? ")
-print("Hello, "+ name)
+print("Hello,", name)
--- pythonprogrammingexercises/exercise_3_odd_even.py
+++ pythonprogrammingexercises/exercise_3_odd_even.py
@@ -11,7 +11,7 @@ def is_odd(number):
     
 def is_even(number):
     text = "Number is even: "
-    if type(number) == int and number % 2 == 0:
+    if type(number) == int and number % 2 == 0 and number != 0:
         return print(text, True)
     elif number == 0:
         return print("Number is 0.")
@@ -23,8 +23,13 @@ is_odd(4)
 is_odd(5)
 is_odd(0)
 is_odd(5.12345)
+is_odd(-5)
+is_odd(-5.12345)
+print()
 
 is_even(4)
 is_even(5)
 is_even(0)
 is_even(5.12345)
+is_even(-4)
+is_even(-5.12345)
