7c7c83830f386f87b4c1c7362a4f029d75f3b97f
Robert Fodor <ingrobertfodor@gmail.com>
2025-03-13 16:44:57 +0100

    Selenium Pytest - TestCase - the-internet.herokuapp.com - Login - Add EXPLICIT WAIT

--- pytest_test_automation_web_login/pages/the_internet_herokuapp.py
+++ pytest_test_automation_web_login/pages/the_internet_herokuapp.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Web_Page:
@@ -26,3 +28,13 @@ class Web_Page:
         pop_up_text = self.browser.find_element(By.ID, "flash-messages").text
         if "You logged into a secure area!" in pop_up_text:
             return "You logged into a secure area!"
+    
+    '''
+    # Same with EXPLICIT WAIT
+    def popup_message(self):
+        wait = WebDriverWait(self.browser, 10)
+        wait.until(EC.visibility_of_element_located((By.ID, "flash-messages")))
+        pop_up_text = self.browser.find_element(By.ID, "flash-messages").text
+        if "You logged into a secure area!" in pop_up_text:
+            return "You logged into a secure area!"'
+    '''
